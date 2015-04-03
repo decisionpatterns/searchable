d4faa933b1076d683e35428ba30b5501bb8737d5
Christopher Brown <cbrown@decisionpatterns.com>
2015-04-03 02:57:10 -0700

    Rename method standard -> std

--- R/stri_detect_std.R
+++ R/stri_detect_std.R
@@ -5,13 +5,13 @@
 #' @param str search target
 #' @param pattern pattern to attempt
 #' @param ... supplementary arguments passed to the underlying functions, 
-#'        including additional settings for \code{stri_opts_standard}
+#'        including additional settings for \code{stri_opts_std}
 #' @param case_insensitive logical; enable simple case insensitive matching
 #'
-#' \code{stri_detect_standard} is equivalent to \code{str %in% pattern} and is 
+#' \code{stri_detect_std} is equivalent to \code{str %in% pattern} and is 
 #' created to provide a parallel to other search methods.
 #'  
-#' \code{stri_opts_standard}
+#' \code{stri_opts_std}
 #' @return 
 #'   logical indicating the matching elements in \code{str}
 #'  
@@ -19,19 +19,19 @@
 #'   \code{\link[stringi]{stri_detect}}
 #' 
 #' @examples 
-#'   stri_detect_standard( letters[1:5], letters[1:2] )
-#'   stri_detect_standard( letters[1:5], LETTERS[1:2] )
-#'   stri_detect_standard( letters[1:5], LETTERS[1:2], opts_standard = list(case_insensitive = TRUE ) )
+#'   stri_detect_std( letters[1:5], letters[1:2] )
+#'   stri_detect_std( letters[1:5], LETTERS[1:2] )
+#'   stri_detect_std( letters[1:5], LETTERS[1:2], opts_std = list(case_insensitive = TRUE ) )
 #'   
-#' @rdname stri_standard            
+#' @rdname stri_std            
 #' @export
 
-stri_detect_standard <- function( str, pattern, ..., opts_standard = NULL ) {
+stri_detect_std <- function( str, pattern, ..., opts_std = NULL ) {
  
   if( ! missing(...) )
-    opts_standard = as.list(opts_standards, ... ) 
+    opts_std = as.list(opts_std, ... ) 
   
-  if( ! is.null(opts_standard$case_insensitive) && opts_standard$case_insensitive ) 
+  if( ! is.null(opts_std$case_insensitive) && opts_std$case_insensitive ) 
     toupper(str) %in% toupper(pattern) else 
     str %in% pattern 
   
@@ -39,13 +39,12 @@ stri_detect_standard <- function( str, pattern, ..., opts_standard = NULL ) {
    
   
 
-#' @rdname stri_standard            
+#' @rdname stri_std            
 #' @export
-stri_opts_standard <- function( case_insensitive = FALSE, ...) { 
+stri_opts_std <- function( case_insensitive = FALSE, ...) { 
 
   opts <- list(...)
   if ( !missing(case_insensitive) ) opts["case_insensitive"] <- case_insensitive
   opts
 
 }
-  
